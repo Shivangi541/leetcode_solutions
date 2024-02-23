05a9f2cac68e1fad6ca00da892dbd73bff20d9af
Shivangi541 <103591063+Shivangi541@users.noreply.github.com>
2024-02-23 23:36:11 +0530

    Time: 148 ms (39.02%), Space: 73.5 MB (45.80%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,23 +1,69 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        // brute force
-        ListNode *temp= head;
-        vector<int>arr;
-        while(temp!=NULL)
+        //If List Contain a Single or 0 Node
+        if(head == NULL || head ->next == NULL)
+            return head;
+        
+        
+        ListNode *temp = NULL;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        // 2 pointer appraoach / turtle-hare Algorithm (Finding the middle element)
+        while(fast !=  NULL && fast -> next != NULL)
         {
-            arr.push_back(temp->val);
-            temp=temp->next;
-        }
-        sort(arr.begin(),arr.end());
-        int i=0;
-        temp=head;
-        while(temp!=NULL)
+            temp = slow;
+            slow = slow->next;          //slow increment by 1
+            fast = fast ->next ->next;  //fast incremented by 2
+            
+        }   
+        temp -> next = NULL;            //end of first left half
+        
+        ListNode* l1 = sortList(head);    //left half recursive call
+        ListNode* l2 = sortList(slow);    //right half recursive call
+        
+        return mergelist(l1, l2);         //mergelist Function call
+            
+    }
+    
+    //MergeSort Function O(n*logn)
+    ListNode* mergelist(ListNode *l1, ListNode *l2)
+    {
+        ListNode *ptr = new ListNode(0);
+        ListNode *curr = ptr;
+        
+        while(l1 != NULL && l2 != NULL)
         {
-            temp->val=arr[i];
-            i=i+1;
-            temp=temp->next;
+            if(l1->val <= l2->val)
+            {
+                curr -> next = l1;
+                l1 = l1 -> next;
+            }
+            else
+            {
+                curr -> next = l2;
+                l2 = l2 -> next;
+            }
+        
+        curr = curr ->next;
+        
         }
-        return head;
+        
+        //for unqual length linked list
+        
+        if(l1 != NULL)
+        {
+            curr -> next = l1;
+            l1 = l1->next;
+        }
+        
+        if(l2 != NULL)
+        {
+            curr -> next = l2;
+            l2 = l2 ->next;
+        }
+        
+        return ptr->next;
     }
 };
