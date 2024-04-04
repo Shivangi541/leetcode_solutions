3036bb768254deb2a570d836dcaa20a4195badb0
Shivangi541 <103591063+Shivangi541@users.noreply.github.com>
2024-04-04 12:06:53 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (87.88%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
-    void recurPermute(vector < int > & ds, vector < int > & nums, vector < vector < int >> & ans, int freq[]) {
-      if (ds.size() == nums.size()) {
-        ans.push_back(ds);
+    void backtrack(vector<vector<int>>& ans, vector<int> &nums, int i, int size){
+    
+    //base case: if the counter reaches the end of the vector
+    if(i==size){
+        //add the temporary vector elements to the big one
+        ans.push_back(nums);
         return;
-      }
-      for (int i = 0; i < nums.size(); i++) {
-        if (!freq[i]) {
-          ds.push_back(nums[i]);
-          freq[i] = 1;
-          recurPermute(ds, nums, ans, freq);
-          freq[i] = 0;
-          ds.pop_back();
-        }
-      }
     }
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
-        vector<int>ds;
-        int freq[nums.size()];
-      for (int i = 0; i < nums.size(); i++) freq[i] = 0;
-      recurPermute(ds, nums, ans, freq);
-      return ans;
+    
+    //loop over the whole vector elements
+    for(int j=i;j<=size;j++){
+        
+        //do swapping
+        swap(nums[i], nums[j]);
+        //recursivelly, backtrack
+        backtrack(ans, nums, i+1, size);
+        //undo
+        swap(nums[i], nums[j]);
     }
+    
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+    
+    //initialize a result vector
+    vector<vector<int>> ans;
+    
+    //call the helper function
+    backtrack(ans, nums, 0, nums.size()-1);
+    
+    //return the final result elements
+    return ans;
+}
+
 };
