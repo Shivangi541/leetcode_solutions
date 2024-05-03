6a18b26d245f9d6d964337079b30b0d58e92828f
Shivangi541 <103591063+Shivangi541@users.noreply.github.com>
2024-05-03 21:31:29 +0530

    Time: 44 ms (68.37%), Space: 17.5 MB (11.07%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // brute force: linear search
-        int row=matrix.size();
-        int col=matrix[0].size();
-       // int i,j;
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-                if(matrix[i][j]==target)
-                    return true;
-            }
-        }
-        return false;
+        int m=matrix.size(),n=matrix[0].size();
+
+	int i=0,j=n-1;     // start from the top right corner. 
+
+	while(i<m&&j>=0){
+		if(matrix[i][j]==target) return true;    // target found 
+
+		if(matrix[i][j]<target)    
+			i++;   // because all the element < jth index in ith row is less than current and
+			          // elements > jth index in ith row are already searched. 
+		else
+			j--;   // all the element < jth index of <ith row are less and 
+			        // jth element of i+1th row is greater than curr.
+	}
+	return false;
     }
 };
