e0c3af9b275578bc311d3b72ca3a33dd76fe9278
Shivangi541 <103591063+Shivangi541@users.noreply.github.com>
2024-05-04 12:23:45 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (44.87%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double solve(double x, long n)
+    {
+        if(n==0)
+        {
+            return 1;
+            
+        }
+        if(n<0)
+        {
+            return solve(1/x, -n);
+        }
+        if(n%2==0)
+        {
+            return solve(x*x, n/2);
+        }
+        else
+        {
+            return x*solve(x*x,(n-1)/2);
+        }
+    }
+    
+    double myPow(double x, int n) {
+        // binary exponentiation
+        return solve(x,(long)n);
+        
+    }
+};
