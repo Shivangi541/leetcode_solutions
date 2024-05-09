adc66c9f3b3d26c2bdba7a14692216314233d7f4
Shivangi541 <103591063+Shivangi541@users.noreply.github.com>
2024-05-09 21:42:13 +0530

    Time: 3 ms (46.83%), Space: 8.1 MB (12.84%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    bool checkValidString(string s) {
-        int leftMin = 0, leftMax = 0;
-
-        for (char c : s) {
-            if (c == '(') {
-                leftMin++;
-                leftMax++;
-            } else if (c == ')') {
-                leftMin--;
-                leftMax--;
-            } else {
-                leftMin--;
-                leftMax++;
-            }
-            if (leftMax < 0) return false;
-            if (leftMin < 0) leftMin = 0;
+     int t[101][101];
+    bool solve(int idx, int open, string& s, int n) {
+        if(idx == n) {
+            return open == 0;
         }
         
-        return leftMin == 0;
+        if(t[idx][open] != -1) {
+            return t[idx][open] == 1 ? true : false;
+        }
+        
+        bool isValid = false;
+        
+        if(s[idx] == '*') {
+            if(open > 0)
+                isValid |= solve(idx+1, open-1, s, n); //Treating * as 
+             isValid |= solve(idx+1, open+1, s, n); //Treating * as (
+            
+            isValid |= solve(idx+1, open, s, n);   //Treating * as ''
+        } else if(s[idx] == '(') {
+            isValid |= solve(idx+1, open+1, s, n);
+        } else if(open > 0) {
+            isValid |= solve(idx+1, open-1, s, n);
+        }
+        
+        return t[idx][open] = isValid;
+    }
+    bool checkValidString(string s) {
+        int n = s.length();
+        memset(t, -1, sizeof(t));
+        return solve(0, 0, s, n);
     }
 };
