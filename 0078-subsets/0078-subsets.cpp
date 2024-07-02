69a156fa3a03c542e299376ab323ce6ce29e1de0
Shivangi541 <103591063+Shivangi541@users.noreply.github.com>
2024-07-02 21:36:02 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (77.80%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,24 @@
 class Solution {
+private:
+    void subset(int ind, vector<int>&nums, vector<int>&ds, vector<vector<int>>&ans){
+        if(ind==nums.size()){
+            ans.push_back(ds); //Not in O(1)
+            return;
+        }
+
+        //subset(ind+1,nums,ds,ans);
+        
+        ds.push_back(nums[ind]); //Pick
+        subset(ind+1,nums,ds,ans);
+        ds.pop_back();
+        subset(ind+1,nums,ds,ans); //Not Pick
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> subs;
-        vector<int> sub;
-        subsets(nums, 0, sub, subs);
-        return subs;
-    }
-private:
-    void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
-        subs.push_back(sub);
-        for (int j = i; j < nums.size(); j++) {
-            sub.push_back(nums[j]);
-            subsets(nums, j + 1, sub, subs);
-            sub.pop_back();
-        }
+        vector<vector<int>>ans;
+        vector<int>ds;
+        subset(0,nums,ds,ans);
+        return ans;
+        
     }
 };
